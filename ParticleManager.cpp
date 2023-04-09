3035bbfa6b52c1ba414e266f0f18cde65b5dacda
沖山 <okiyama1609@outlook.jp>
2023-04-10 00:42:39 +0900

    ひとまず

--- ParticleManager.cpp
+++ ParticleManager.cpp
@@ -1,12 +1,33 @@
 #include "ParticleManager.h"
 ID3D12Device* ParticleManager::device = nullptr;
 ID3D12GraphicsCommandList* ParticleManager::commandList;
+PipelineSet ParticleManager::ParPipeline;
+ComPtr<ID3D12Resource> ParticleManager::vertBuff;
+D3D12_VERTEX_BUFFER_VIEW ParticleManager::vbView{};
+ParticleManager::VertexPos ParticleManager::vertices[vertexCount];
 
-void ParticleManager::StaticInitialize(ID3D12Device* device, int window_width, int window_height)
+const DirectX::XMFLOAT3 operator+(const DirectX::XMFLOAT3& lhs, const DirectX::XMFLOAT3& rhs)
+{
+	XMFLOAT3 result;
+	result.x = lhs.x + rhs.x;
+	result.y = lhs.y + rhs.y;
+	result.z = lhs.z + rhs.z;
+
+	return result;
+
+}
+
+ParticleManager::ParticleManager(uint32_t handle)
+{
+	tex = TextureManager::GetTextureData(handle);
+}
+
+void ParticleManager::StaticInitialize(ID3D12Device* device)
 {
 	assert(device);
 	ParticleManager::device = device;
 	ParPipeline = Pipeline::CreateParticlePipline(device);
+	CreateModel();
 
 }
 
@@ -17,7 +38,7 @@ void ParticleManager::PreDraw(ID3D12GraphicsCommandList* cmdList)
 	commandList->SetPipelineState(ParPipeline.pipelineState.Get());
 	commandList->SetGraphicsRootSignature(ParPipeline.rootSignature.Get());
 	// プリミティブ形状の設定コマンド
-	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // 三角形リスト
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_POINTLIST); // 三角形リスト
 }
 
 void ParticleManager::PostDraw()
@@ -25,7 +46,155 @@ void ParticleManager::PostDraw()
 	ParticleManager::commandList = nullptr;
 }
 
-ParticleManager* ParticleManager::Create(uint32_t Handle)
+void ParticleManager::CreateModel()
 {
-	return nullptr;
+	HRESULT result = S_FALSE;
+
+	vector<VertexPos> realVertices;
+
+
+	unsigned short indicesSquare[] = {
+		0,1,2,
+		2,1,3,
+	};
+
+	//std::copy(std::begin(indicesSquare), std::end(indicesSquare), indices);
+
+	UINT sizeVB = static_cast<UINT>(sizeof(vertices));
+
+	D3D12_HEAP_PROPERTIES heapProp{};
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeVB;
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	result = device->CreateCommittedResource(
+		&heapProp,
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff)
+	);
+	assert(SUCCEEDED(result));
+
+
+	// 頂点バッファへのデータ転送
+	VertexPos* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	if (SUCCEEDED(result))
+	{
+		memcpy(vertMap, vertices, sizeof(vertices));
+		//memcpy(vertMap, vertices, sizeof(vertices));
+		vertBuff->Unmap(0, nullptr);
+	}
+
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+	vbView.SizeInBytes = sizeof(vertices);
+	vbView.StrideInBytes = sizeof(vertices[0]);
 }
+
+ParticleManager* ParticleManager::Create(uint32_t Handle, WorldTransform* Wt)
+{
+	// Spriteのインスタンスを生成
+	ParticleManager* pat = new ParticleManager(Handle);
+
+	if (pat == nullptr) {
+		return nullptr;
+	}
+
+	// 初期化
+	if (!pat->Initialize(Wt)) {
+		delete pat;
+		assert(0);
+		return nullptr;
+	}
+
+	return pat;
+}
+
+bool ParticleManager::Initialize(WorldTransform* Wt)
+{
+	wt = Wt;
+	wt->CreateConstBuffer(device);
+	
+	return true;
+}
+
+void ParticleManager::Update(ViewProjection* camera)
+{
+
+	HRESULT result;
+
+	particle.remove_if([](Particle& x) {return x.frame >= x.num_frame; });
+
+	for (std::forward_list<Particle>::iterator it = particle.begin(); it != particle.end(); it++)
+	{
+		it->frame++;
+
+		it->velocity = it->velocity + it->accel;
+
+		it->position = it->position + it->velocity;
+
+		float f = (float)it->frame / it->num_frame;
+
+		it->scale = (it->e_scale - it->s_scale) * f;
+		it->scale += it->s_scale;
+	}
+
+
+
+	VertexPos* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	if (SUCCEEDED(result))
+	{
+		for (std::forward_list<Particle>::iterator it = particle.begin(); it != particle.end(); it++)
+		{
+			vertMap->pos = it->position;
+
+			vertMap->scale = it->scale;
+
+			vertMap++;
+		}
+		vertBuff->Unmap(0, nullptr);
+	}
+
+	wt->Map();
+
+	wt->UpdateMatrixBill(camera);
+}
+
+void ParticleManager::Draw()
+{
+	commandList->IASetVertexBuffers(0, 1, &vbView);
+
+	commandList->SetDescriptorHeaps(1, tex->srvHeap.GetAddressOf());
+	D3D12_GPU_DESCRIPTOR_HANDLE srvGpuHandle = tex->gpuHandle;
+
+	commandList->SetGraphicsRootDescriptorTable(1, srvGpuHandle);
+
+	commandList->SetGraphicsRootConstantBufferView(0, wt->constBuffB0->GetGPUVirtualAddress());
+
+	commandList->DrawInstanced((UINT)std::distance(particle.begin(), particle.end()), 1, 0, 0);
+
+}
+
+void ParticleManager::Add(int life, XMFLOAT3 position, XMFLOAT3 velocity, XMFLOAT3 accel, float start_scale, float end_scale)
+{
+	particle.emplace_front();
+
+	Particle& p = particle.front();
+
+	p.position = position;
+	p.velocity = velocity;
+	p.accel = accel;
+	p.num_frame = life;
+}
+
+
--- WorldTronsform.cpp
+++ WorldTronsform.cpp
@@ -72,6 +72,81 @@ void WorldTransform::UpdateMatrix(XMMATRIX view, XMMATRIX projection,XMFLOAT3 ca
 	
 }
 
+void WorldTransform::UpdateMatrixBill(ViewProjection* camera)
+{
+	XMMATRIX matScale, matRot, matTrans;
+
+	// スケール、回転、平行移動行列の計算
+	matScale = XMMatrixScaling(scale_.x, scale_.y, scale_.z);
+	matRot = XMMatrixIdentity();
+	matRot *= XMMatrixRotationZ(rotation_.z);
+	matRot *= XMMatrixRotationX(rotation_.x);
+	matRot *= XMMatrixRotationY(rotation_.y);
+	matTrans = XMMatrixTranslation(translation_.x, translation_.y, translation_.z);
+
+	// ワールド行列の合成
+	matWorld_ = XMMatrixIdentity(); // 変形をリセット
+	matWorld_ *= matScale;          // ワールド行列にスケーリングを反映
+	matWorld_ *= matRot;            // ワールド行列に回転を反映
+	matWorld_ *= matTrans;          // ワールド行列に平行移動を反映
+
+	XMVECTOR eyePosition = XMLoadFloat3(&camera->Geteye());
+
+	XMVECTOR targetPosition = XMLoadFloat3(&camera->Gettarget());
+
+	XMVECTOR upVector = XMLoadFloat3(&camera->Getup());
+
+	XMVECTOR cameraAxisZ = XMVectorSubtract(targetPosition, eyePosition);
+
+	cameraAxisZ = XMVector3Normalize(cameraAxisZ);
+
+	XMVECTOR cameraAxisX;
+
+	cameraAxisX = XMVector3Cross(upVector, cameraAxisZ);
+
+	cameraAxisX = XMVector3Normalize(cameraAxisX);
+
+	XMVECTOR cameraAxisY;
+
+	cameraAxisY = XMVector3Cross(cameraAxisZ, cameraAxisX);
+
+	XMMATRIX matCameraRot;
+
+	matCameraRot.r[0] = cameraAxisX;
+	matCameraRot.r[1] = cameraAxisY;
+	matCameraRot.r[2] = cameraAxisZ;
+	matCameraRot.r[3] = XMVectorSet(0, 0, 0, 1);
+
+	XMMATRIX matView = XMMatrixTranspose(matCameraRot);
+
+	XMVECTOR reverseEyePosition = XMVectorNegate(eyePosition);
+
+	XMVECTOR tX = XMVector3Dot(cameraAxisX, reverseEyePosition);
+	XMVECTOR tY = XMVector3Dot(cameraAxisY, reverseEyePosition);
+	XMVECTOR tZ = XMVector3Dot(cameraAxisZ, reverseEyePosition);
+
+	XMVECTOR translation = XMVectorSet(tX.m128_f32[0], tY.m128_f32[1], tZ.m128_f32[2], 1.0f);
+
+	matView.r[3] = translation;
+
+	matBillboard.r[0] = cameraAxisX;
+	matBillboard.r[1] = cameraAxisY;
+	matBillboard.r[2] = cameraAxisZ;
+	matBillboard.r[3] = XMVectorSet(0, 0, 0, 1);
+
+	//// 親行列の指定がある場合は、掛け算する
+	//if (parent_) {
+	//	matWorld_ *= parent_->matWorld_;
+	//}
+
+	// 定数バッファに書き込み
+	constMap->viewproj = matView * camera->GetMatProjection();
+	constMap->world = matBillboard;
+	constBuffB0->Unmap(0, nullptr);
+	
+
+}
+
 void WorldTransform::UpdateSpriteMatrix(XMMATRIX projection)
 {
 	XMMATRIX matScale, matRot, matTrans;
