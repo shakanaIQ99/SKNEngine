3b17d9b4c43e1ca9f8bd295b83c69bbac192140c
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2023-12-13 16:04:01 +0900

    地雷形だけ

---
+++ SourceCode/App/Enemy/EnemyMine.cpp
@@ -0,0 +1,1 @@
+#include "EnemyMine.h"
---
+++ SourceCode/App/Enemy/EnemyMine.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "StuructTransform.h"
+using namespace DirectX;
+
+class EnemyMine:public StuructTransform
+{
+public:
+
+	EnemyMine();
+	~EnemyMine();
+
+	static void SetModel(ObjModel* _model);
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	/// <param name="velocity">速度</param>
+	void Initlize(const XMFLOAT3& position, const XMFLOAT3& rota, const XMFLOAT3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw();
+
+	bool IsDead()const { return isDead_; }
+
+	//衝突を検出したら呼び出されるコールバック関数
+	void OnCollision();
+
+	XMFLOAT3 GetWorldPosition() { return St->Wt.translation_; }
+	XMFLOAT3 GetScale() { return St->Wt.scale_; }
+	XMFLOAT3 GetVec() { return Velocity_; }
+
+
+private:
+
+	static std::unique_ptr<ObjModel> Premodel;
+
+	//モデル
+	//StuructTransform bullet;
+
+
+	//テクスチャハンドル
+
+
+	//速度
+	XMFLOAT3 Velocity_;
+
+	//寿命<frm>
+	static const int32_t kLifeTime = 60 * 5;
+
+	//デスタイマー
+	int32_t deathTimer_ = kLifeTime;
+
+	//デスフラグ
+	bool isDead_ = false;
+
+
+
+};
+
