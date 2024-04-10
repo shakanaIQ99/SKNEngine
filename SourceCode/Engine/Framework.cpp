72c65ae735ba71aa50a842e02910280ed32c1399
沖山 <okiyama1609@outlook.jp>
2024-04-10 18:26:01 +0900

    フレームワーク用意

---
+++ SourceCode/Engine/Framework.cpp
@@ -0,0 +1,30 @@
+#include "Framework.h"
+
+void Framework::Initialize()
+{
+}
+
+void Framework::Destroy()
+{
+}
+
+void Framework::Update()
+{
+}
+
+bool& Framework::GetEndRequest()
+{
+	// TODO: return ステートメントをここに挿入します
+}
+
+void Framework::SetWindowData(const std::string& title, const float width, const float height)
+{
+}
+
+void Framework::SetWindowColor(const Float4& color)
+{
+}
+
+void Framework::Run()
+{
+}
---
+++ SourceCode/Engine/Framework.h
@@ -0,0 +1,75 @@
+#pragma once
+#include"DxWindow.h"
+#include"DirectXCommon.h"
+#include"OBJ3D.h"
+#include"Input.h"
+#include<wrl.h>
+#include"LightGroup.h"
+#include<sstream>
+#include<iomanip>
+#include"ImGuiManager.h"
+#include"FPS.h"
+#include"ParticleManager.h"
+#include"BulletManager.h"
+#include"PostEffect.h"
+#include"SpriteCommon.h"
+#include"Draw3DLine.h"
+#include"AudioManager.h"
+#include"myMath.h"
+
+class Framework
+{
+protected:
+
+	std::wstring title = L"";
+	float windowWidth = 0.0f;//横幅
+	float windowHeight = 0.0f;//縦幅
+
+	Float4 windowColor = { 0.0f ,0.0f ,0.0f ,0.0f };
+
+	bool endRequest_ = false;
+
+	std::unique_ptr<DxWindow> dxWindow;
+	std::unique_ptr<FPS>fps;
+
+public:
+
+	virtual ~Framework() = default;
+
+	//初期化処理
+	virtual void Initialize();
+
+	//終了処理
+	virtual void Destroy();
+
+	//更新処理
+	virtual void Update();
+
+	//描画処理
+	virtual void SceneDraw() = 0;
+	virtual void PostEffectDraw() = 0;
+
+	//終了フラグの取得
+	virtual bool& GetEndRequest();
+
+
+public:
+
+	/// <summary>
+	/// ウィンドウの設定
+	/// </summary>
+	/// <param name="title">タイトルバーの名前</param>
+	/// <param name="width">ウィンドウの横幅</param>
+	/// <param name="height">ウィンドウの縦幅</param>
+	void SetWindowData(const std::string& title = "DirectXGame", const float width = 1280.0f, const float height = 720.0f);
+
+	/// <summary>
+	/// ウィンドウの色を変更
+	/// </summary>
+	/// <param name="color"></param>
+	void SetWindowColor(const Float4& color = { 0.1f,0.25f,0.5f,0.0f });
+
+	//実行処理
+	void Run();
+};
+
