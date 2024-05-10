5fc4d8159ee03a81a87ec60bff15ed195fa12828
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2024-05-10 11:27:17 +0900

    一旦

--- SourceCode/Engine/BehaviorTree/SelectorNode.cpp
+++ SourceCode/Engine/BehaviorTree/SelectorNode.cpp
@@ -6,7 +6,41 @@ void SelectorNode::OnStart()
 
 NodeStatus SelectorNode::Update()
 {
-	return NodeStatus();
+	if (children.size() < 0)
+	{
+		InitNode();
+		return NodeStatus::Success;
+	}
+
+	if (selectedIndex < 0)
+	{
+		//selectedIndex = Util::RNG(0, (int32_t)children.size() - 1, true);
+	}
+
+	NodeStatus childStatus = NodeStatus::Inactive;
+	int32_t ind = 0;
+	for (auto itr = children.begin(); itr != children.end(); itr++)
+	{
+		if (ind == selectedIndex)
+		{
+			childStatus = (*itr)->Update();
+		}
+		ind++;
+	}
+
+	if (childStatus == NodeStatus::Completed || childStatus == NodeStatus::Success)
+	{
+		InitNode();
+		return NodeStatus::Success;
+	}
+
+	if (childStatus == NodeStatus::Failure)
+	{
+		InitNode();
+		return NodeStatus::Failure;
+	}
+
+	return NodeStatus::Running;
 }
 
 void SelectorNode::OnEnd()
